17579b730b4c720908b9dd7a5c8ed859f9be05d6
Daniele Silva <danielepereira9503@gmail.com>
2024-02-28 21:46:45 +0000

    task 3

--- polls/routes.js
+++ polls/routes.js
@@ -1,10 +1,73 @@
 const express = require("express");
 const router = express.Router();
 const services = require("./services");
+const { pollsCollection } = require("../db/mongodb");
+const { isSchema } = require("joi");
+const { createPollSchema, voteSchema } = require("./schemas");
 
 router.get("/", async (req, res) => {
   const polls = await services.getAllPolls();
   res.status(200).json(polls);
+});
+// tarefa 1
+router.get("/:id", async (req, res) => {
+  const poll = await services.getPollById(req.params.id);
+  if (!poll) {
+    return res.status(404).json({ error: "poll not found" });
+  }
+  res.status(200).json(poll);
+});
+
+//tarefa 2 Poll criada
+router.post("/", async (req, res) => {
+  const { error, value } = createPollSchema.validate(req.body);
+  if (error) {
+    console.log("erro na validação", error);
+    return res.status(400).json(error.details);
+  }
+
+  console.log("Dados válidos: ", value);
+
+  value.options = value.options.map((opt) => {
+    return {
+      name: opt,
+      votes: 0,
+    };
+  });
+
+  const createPollResult = await services.createPoll({
+    question: value.question,
+    options: value.options,
+  });
+
+  const result = await services.getPollById(createPollResult.insertedId);
+  res.status(201).json(result);
+});
+
+//tarefa 3
+router.post("/:pollId/vote", async (req, res) => {
+  const { error, value } = voteSchema.validate(req.body);
+  if (error) {
+    return res.status(404).json(error.details);
+  }
+
+  const poll = await services.getPollById(req.params.pollId);
+  if (!poll) {
+    return res.status(404).json({ error: "poll not found" });
+  }
+  const optionExists = poll.options.some(
+    (option) => option.name === value.option
+  );
+  if (!optionExists) {
+    return res.status(400).json({ error: "invalid option" });
+  }
+
+  const updatedPoll = await services.voteOption(
+    req.params.pollId,
+    value.option
+  );
+
+  res.status(200).json(updatedPoll);
 });
 
 router.delete("/:id", async (req, res) => {
--- polls/services.js
+++ polls/services.js
@@ -1,5 +1,34 @@
 const db = require("../db/mongodb");
 
+async function createPoll(value) {
+  try {
+    return await db.getDB().collection(db.pollsCollection).insertOne(value);
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+async function voteOption(pollId, option) {
+  try {
+    const result = await db
+      .getDB()
+      .collection(db.pollsCollection)
+      .updateOne(
+        { _id: db.toMongoID(pollId), "options.name": option },
+        { $inc: { "options.$.votes": 1 } }
+      );
+    if (result.modifiedCount > 0) {
+      return await getPollById(pollId);
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
 async function getPollById(pollId) {
   try {
     return await db
@@ -39,4 +68,6 @@ module.exports = {
   getPollById,
   getAllPolls,
   deletePollById,
+  createPoll,
+  voteOption,
 };
